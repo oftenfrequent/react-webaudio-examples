2d80b8758660bcab5c5688013cadcbc2cf8cc886
k23Burke <k23burke@gmail.com>
2017-10-12 00:17:30 -0400

    example sources file

---
+++ src/assets/sources.example.js
@@ -0,0 +1,8 @@
+import reverb from './BatteryBenson.wav';
+
+export default {
+  turntableLeft: 'https://freemusicarchive.org/file/music/no_curator/People_Skills/Gunshots_at_Crestridge_2017_lp_Blackest_Ever_Black/People_Skills_-_01_-_Obstinate_Truss.mp3',
+  turntableRight: 'https://freemusicarchive.org/file/music/ccCommunity/Soularflair/Soundtrack_Sampler_FILMTVGAMES/Soularflair_-_19_-_Static_Sea.mp3',
+  simpleSong: 'https://freemusicarchive.org/file/music/Section_27/Illocanblo/Cryptovirus/Illocanblo_-_01_-_Ruby_To_MIDI_Lows.mp3',
+  reverbImpulse: reverb,
+}
